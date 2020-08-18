c846a50aed4406247436e1989e6b5f767ded832b
Briana Oniru <damioniru16@gmail.com>
2020-08-18 03:55:52 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string="Sqauwk!")
+def parrot(string = "Sqauwk!")
   puts "#{string}"
   return "#{string}"
 end
